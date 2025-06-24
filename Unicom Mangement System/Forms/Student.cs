f3069bebffec8e60573822899efbd4e7b56b75d3
Deneesan-1 <deneesan18@gmail.com>
2025-06-24 08:52:11 +0530

    Initial commit

---
+++ Unicom Mangement System/Forms/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unicom_Mangement_System.Forms
+{
+    public partial class Student : Form
+    {
+        public Student()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
